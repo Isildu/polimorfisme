f6c09a29c81ec603e5efa70a27505d5177301485
Oriol Canellas <oriolcanellas3@gmail.com>
2026-04-15 12:09:42 +0200

    sss

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-class Personaje{
-    #nombre
-    #vida
-    constructor(nombre, vida){
-        this.nombre = 
+class Narciso {
+    esNarcisista(num) {
+        let digitos = num.toString();
+        let potencia = digitos.length;
+        let suma = 0;
+        for (let x = 0; x < potencia; x++) {
+           suma += Math.pow(parseInt(digitos[x]), potencia);
+        }
+        return suma === num;
     }
+
 }
+
+const n = new Narciso();
+console.log(n.esNarcisista(153));
+console.log(n.esNarcisista(123));
