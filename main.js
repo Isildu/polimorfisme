1c8c8ac776ce3b5268e0f58033d0a8c032130d24
Oriol Canellas <oriolcanellas3@gmail.com>
2026-04-15 12:46:43 +0200

    aaaaaaaaaa

--- main.js
+++ main.js
@@ -14,3 +14,41 @@ class Narciso {
 const n = new Narciso();
 console.log(n.esNarcisista(153));
 console.log(n.esNarcisista(123));
+
+class Personaje {
+    #nombre
+    #vida
+    constructor(nombre, vida){
+        this.#nombre = nombre;
+        this.#vida = vida;
+    }
+    atacar(){
+
+    }
+    recibirDanyo(cantidad){
+        if(cantidad < this.#vida){
+            this.#vida -= cantidad
+        }else this.#vida = 0
+    }
+    estarVivo(){
+        if (vida > 0){
+            return true
+        } else return false
+    }
+}
+
+class Guerrero extends Personaje {
+    #fuerza
+     constructor(nombre, vida, fuerza) {
+        super(nombre, vida);
+        this.#fuerza = fuerza;
+    }
+}
+
+class Mago extends Personaje {
+ #mana
+     constructor(nombre, vida, mana) {
+        super(nombre, vida);
+        this.#mana = mana;
+    }
+}
